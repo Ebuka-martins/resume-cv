2a7a7e5d52aadf7a3614e7c464c51934246bddb9
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-11-28 02:55:50 +0000

    add git ignore and update interest.html

---
+++ assets/js/maps.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+function initMap() {
+const centerLocation = { lat: 53.441769, lng: -8.453751 };
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 3,
+  center: centerLocation,   
+});
+
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const locations = [
+  { lat: 53.426449, lng: -6.24991 },
+  { lat: 51.925388, lng: -8.46995 },
+  { lat: 51.83589, lng: -8.26343 },
+];
+
+const markers = locations.map((location, i) => {
+  return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+  });
+});
+
+new MarkerClusterer(map, markers, {
+  imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
+}
